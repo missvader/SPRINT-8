933fa791bc54842253a3f99c01d7a7301d5b8c0b
missvader <vanepalazon611@gmail.com>
2022-12-12 15:29:32 +0100

    install daisyui. Header with logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from "react";
 import logoStarWars from "../assets/starwars-logo-3.png";
 
 const Header = () => {
+  /*TODO: make it responsive */
   return(
-
-    <div className="grid grid-cols-6  mx-auto">
-      <div className="col-start-3 col-end-5 grid justify-items-center">
+    <div className="navbar bg-transparent">
+      <div className="navbar-start">
+      </div>
+      <div className="navbar-center">
         <img src ={logoStarWars} alt="star wars logo" className="w-60"/>
       </div>
-      <div className="col-start-6 flex items-center justify-around">
-        <button className="text-white ">LOGIN</button>
-        <button className="text-white ">SIGN UP</button>
+      <div className="navbar-end mr-3">
+        <a className="link link-hover text-gray-400 font-bold mx-5">LOG IN</a>
+      <div className="text-neutral-800">//</div>
+        <a className="link link-hover text-slate-400 font-bold mx-5">SIGN UP</a>
       </div>
     </div>
   );
