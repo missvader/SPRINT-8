115378ff6e51d71930095acd1a40d2efb94b1e2a
missvader <vanepalazon611@gmail.com>
2022-12-23 19:54:26 +0100

    more layout changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,15 +3,17 @@ import {NavLink } from "react-router-dom";
 
 const Menu = () =>{
   return (
-    <nav className="navbar flex justify-center border-white border-b">
+    <nav className=" flex justify-center border-white border-b pb-2">
       <div className="px-3">
-        <NavLink to="/" className={({isActive}) => (isActive ? "border-b-2 border-yellow-100" : "text-zinc-400")}>
-          <button className="text-zinc-400 hover:text-white">HOME</button>
+        <NavLink to="/" className={({isActive}) => (
+            isActive ? "border-b-2 border-yellow-100 text-white" : "text-zinc-400")}>
+          <button className=" hover:text-white">HOME</button>
         </NavLink>
       </div>
       <div className="px-3">
-        <NavLink to="/starships" className={({isActive}) => (isActive ? "border-b-2 border-yellow-100" : "text-zinc-400")}>
-          <button className="text-zinc-400 hover:text-white">STARSHIPS</button>
+        <NavLink to="/starships" className={({isActive}) => (
+            isActive ? "border-b-2 border-yellow-100 text-white" : "text-zinc-400")}>
+          <button className=" hover:text-white">STARSHIPS</button>
         </NavLink>
       </div>   
         
